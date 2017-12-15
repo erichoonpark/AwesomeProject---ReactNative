57eb39af9782f550f213cc7a9d430a44f773d5a6
Eric Park <erichoonpark@gmail.com>
2017-12-14 16:14:11 -0800

    Load luxerone.com page

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component, StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <WebView
+        source={{uri: 'https://app.luxerone.com/account/history'}}
+        style={{marginTop: 20}}
+      />
     );
   }
 }
